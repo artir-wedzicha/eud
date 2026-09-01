87c3f48d061b90da3bc74f3a781174ec44b6eea3
arturwedzicha <51323548+arturwedzicha@users.noreply.github.com>
2026-09-01 07:35:55 +0200

    test_html

---
+++ embeds/cdn/env-config.js
@@ -0,0 +1,19 @@
+window._env_ = {
+  ANYCAST_APP_ID: 'B499DC22',
+  API_URL: '/api',  // PROXY to https://api.trueloyal.com (PRODUCTION)
+  BASE_URL: 'https://advocacy.trueloyal.com/',  // PRODUCTION
+  DOMAIN: 'trueloyal.com',  // PRODUCTION
+  CHROMECAST_APP_ID: '4F72DEEC',
+  CLIENT_ID: '14fb70207826fe380c9b7d97fa0ed6df43d25ac622b666127037a403d7742aa4',
+  FACEBOOK_CLIENT_ID: '1269621286527596',
+  FILESTACK_API_KEY: 'Aoba6jBccRfypjdfz6rPLz',
+  HUBSPOT_ID: '39604733',
+  KINESIS_ENDPOINT_OVERRIDE: 'http://localhost:4566',
+  LINKEDIN_CLIENT_ID: '817q4xw96vw6es',
+  NODE_ENV: 'development',
+  PUSHER_KEY: 'e888d59ed1b28ec834d9',
+  STRIPE_PUBLISHABLE_KEY: 'pk_test_yML6g9jrgBk7Qkx0wiCZRhSr',
+  DROPKIQ_KEY: 'ad496710-92cd-013d-0a91-4a36325215e0',
+  EUD_BUNDLE_URL: 'https://cdn.hypemarks.com/assets/eud-wrapper.js',
+  EUD_SCRIPT_URL: 'https://cdn.hypemarks.com/assets/tl-eud-dashboard.js',
+};
